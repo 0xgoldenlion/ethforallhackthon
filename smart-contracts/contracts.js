7fd0ed5024f13f351606994b710c0b1aedfc03e6
malawadd <100777093+0xgoldenlion@users.noreply.github.com>
2023-02-18 19:08:33 +0300

    contracts deployed on mantle

---
+++ smart-contracts/contracts.js
@@ -0,0 +1,8 @@
+
+    export const SudokuVerifier = "0xdd545adDce7fbC82C02483BF6c8cAdc6AD70c85A"
+    export const Sudoku = "0x1000ce4BD3fdb281851f76aB76D79eCe2782c3D1"
+    export const FutoshikiVerifier = "0x89086E8e5AE2fF9260F3ef469f31aFc1E36D0191"
+    export const Futoshiki = "0x7aC7fF2DA3B22f8E219FF94c84712cAb7Ba98fe7"
+    export const SkyscrapersVerifier = "0xc6fFB6d0d778AE438f455aA59ADF73666A85D8F1"
+    export const Skyscrapers = "0xEC050485fFf47C2308867A1A9CeFD7669c61563C"
+    
