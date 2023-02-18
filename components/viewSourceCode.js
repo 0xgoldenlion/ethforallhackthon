5564a0bc0652176ffe08ddcdcce4597fe60f7584
malawadd <100777093+0xgoldenlion@users.noreply.github.com>
2023-02-18 19:21:29 +0300

    update

--- components/viewSourceCode.js
+++ components/viewSourceCode.js
@@ -1,4 +1,4 @@
-const GITHUB_LINK = "https://github.com/0xgoldenlion/ethforallhackthon";
+const GITHUB_LINK = "https://github.com/0xgoldenlion/comma";
 
 export default function ViewSourceCode() {
   return (
